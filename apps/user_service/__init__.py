1dd0b98f7c06d9373b4abc631d19e52c19aef845
Samson Ajifowoke <samsonajifowoke@gmail.com>
2026-07-25 09:41:13 +0100

    chore: database and user models setup

---
+++ apps/user_service/__init__.py
@@ -0,0 +1,8 @@
+from apps.user_service.app.api.models.base import Base
+from apps.user_service.app.api.models.users import User
+
+
+__all__ = [
+    "Base",
+    "User"
+]
---
+++ apps/user_service/app/api/models/users.py
@@ -0,0 +1,55 @@
+import enum
+import uuid
+from datetime import datetime, timezone
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import (
+    String,
+    Boolean,
+    text,
+    UUID,
+    DateTime,
+    PrimaryKeyConstraint,
+    Index,
+    Enum,
+)
+
+from apps.user_service.app.api.models.base import Base
+
+
+class UserType(str, enum.Enum):
+    EMAIL = "email"
+    GOOGLE = "google"
+
+
+class UserRole(str, enum.Enum):
+    USER = "user"
+    ADMIN = "admin"
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID, server_default=text("uuid_generate_v7()")
+    )
+    type: Mapped[UserType] = mapped_column(
+        Enum(UserType, values_callable=lambda e: [m.value for m in e])
+    )
+    role: Mapped[UserRole] = mapped_column(
+        Enum(UserRole, values_callable=lambda e: [m.value for m in e])
+    )
+    email: Mapped[str | None] = mapped_column(String, unique=True)
+    hashed_password: Mapped[str | None] = mapped_column(String)
+    google_id: Mapped[str | None] = mapped_column(String)
+    google_email: Mapped[str | None] = mapped_column(String, unique=True)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_verified: Mapped[bool] = mapped_column(Boolean, default=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), default=lambda: datetime.now(timezone.utc)
+    )
+
+    __table_args__ = (
+        PrimaryKeyConstraint("id", name="users_pk"),
+        Index("idx_users_email", email),
+        Index("idx_users_google_email", google_email),
+    )
